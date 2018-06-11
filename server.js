55e01dd89793e84ce2ed03a7cbc1209f3d78fc81
Diego M. Martínez Ayuso <diego.martinez.ayuso@gmail.com>
2018-06-11 08:12:28 +0200

    modified config server express and add some styles

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/rick-morty-app'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/*', function(req,res) {    
+    res.sendFile(path.join(__dirname+'/dist/rick-morty-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
